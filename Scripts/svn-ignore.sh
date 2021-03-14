f98d65b1050382cee99c9514ea7298354fc6b17d
João Lourenço <joao.lourenco@fct.unl.pt>
2021-03-14 22:33:05 +0000

    Moved Bibliography to its own folder

--- Scripts/svn-ignore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-STDIGN="`dirname $0`/latex-ignore.txt"
-LOCALIGN=latex-ignore.txt
-
-[ -f "$STDIGN" ] && svn propset svn:ignore -F "$STDIGN" $@
-[ -f "$LOCALIGN" ] && svn propset svn:ignore -F "$LOCALIGN" $@
