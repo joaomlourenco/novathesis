cd5a635206fc14882b374981a2a318f6a2579d76
João Lourenço <joao.lourenco@fct.unl.pt>
2021-04-19 00:37:46 +0100

    Fixed script make covers

--- Scripts/make-covers.sh
+++ Scripts/make-covers.sh
@@ -2,14 +2,14 @@ uminho=xe
 esep=xe
 
 NOW=$(date "+%Y-%m-%d@%H:%M:%S")
-OUTDIR=$NOW/Covers
+OUTDIR=../$NOW/Covers
 
 rm -rf OUTDIR
 
 cp Config/1_novathesis.tex Config/1_novathesis.bak
 for DEGREE in msc phd; do
     mkdir -p $OUTDIR/$DEGREE
-    perl -pi -e "s-(%\s*)?\\\\ntsetup\{docDEGREE=.*\}-\\\\ntsetup{docDEGREE=${DEGREE}}-" Config/1_novathesis.tex
+    perl -pi -e "s-(%\s*)?\\\\ntsetup\{docdegree=.*\}-\\\\ntsetup{docdegree=${DEGREE}}-" Config/1_novathesis.tex
     find NOVAthesisFiles/Schools -type d -depth 2 | fgrep -v Images | cut -d '/' -f 3- | while read i; do
     	perl -pi -e "s-(%\s*)?\\\\ntsetup\{school=.*\}-\\\\ntsetup{school=$i}-" Config/1_novathesis.tex
         U=$(echo $i | cut -d '/' -f 1)
